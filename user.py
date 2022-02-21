dd9b415e17294f4030e5245e5e39c464cab2380b
felo <felix.kurgat@student.moringaschool.com>
2022-02-21 11:45:38 +0300

    added class init

--- user.py
+++ user.py
@@ -2,9 +2,9 @@ class User:
     """
     User class for generating new instances of users.
     """
-    
+    users_list = []
 
-    def __init__(self, username, password):
+    
         """
         __init__ method that defines properties of our users objects.
         """
