3bcee9056b284d3f3793b8e7b029bd1839965503
felo <felix.kurgat@student.moringaschool.com>
2022-02-21 11:46:11 +0300

    added class save user

--- user.py
+++ user.py
@@ -4,14 +4,14 @@ class User:
     """
     users_list = []
 
-    
+    def __init__(self, username, password):
         """
         __init__ method that defines properties of our users objects.
         """
         self.username = username
         self.password = password
 
-    def save_user(self):
+    
         """
         This is a save method for saving newly created objects.
         """
